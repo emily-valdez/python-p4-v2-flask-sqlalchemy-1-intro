2448eff7d9bcff83414d18ec02f1fe2c3b0af4ee
Emily Valdez <emily.milan.valdez@gmail.com>
2023-11-24 15:20:18 -0600

    solution

--- server/models.py
+++ server/models.py
@@ -1,3 +1,11 @@
+#A model class, also referred to simply as a model, is a Python class that (1) defines a new Python data type, and (2) defines database metadata to describe an SQL table. We can treat a model class like any other Python class, meaning we can create instances, invoke methods, etc. Additionally, Flask-SQLAlchemy performs object-relational mapping between the model class and an associated database table, while Flask-Migrate uses changes to the model class to evolve a database schema.
+
+#Defining a model with Flask-SQLAlchemy requires a Python class with four key traits:
+    #Inheritance from the db.Model class.
+    #A __tablename__ class attribute.
+    #One or more class attributes assigned to the type db.Column.
+    #One class attribute specified to be the table's primary key.
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
